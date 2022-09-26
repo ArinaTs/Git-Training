cf21e87aa37ca48d6e0372e13c5ba125252f2fb6
ArinaTs <GnezdoBobra@yandex.ru>
2022-09-26 19:03:10 +0300

    отрицательный ноль не наблюдала, т. ч. добавлено норм описание

--- TrainingSolution/TrainingProject_DivOperation/div_operation.c
+++ TrainingSolution/TrainingProject_DivOperation/div_operation.c
@@ -2,13 +2,14 @@
 int main() {
 	int a, b;
 	float div;
+	printf("Input two number: ");
 	scanf_s("%d %d", &a, &b);
 	if (b == 0) {
 		printf("Error");
 	}
 	else {
 		div = a / b;
-		printf("%d", div);
+		printf("a / b = %f\n", div);
 	}
 	system("pause");
 	return 0;
