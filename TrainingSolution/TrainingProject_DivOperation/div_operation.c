457b811fe0d401fb329fb5f92450a6defe898a17
ArinaTs <GnezdoBobra@yandex.ru>
2022-09-26 18:57:01 +0300

    исправлен баг с делением на ноль

--- TrainingSolution/TrainingProject_DivOperation/div_operation.c
+++ TrainingSolution/TrainingProject_DivOperation/div_operation.c
@@ -3,8 +3,13 @@ int main() {
 	int a, b;
 	float div;
 	scanf_s("%d %d", &a, &b);
-	div = a / b;
-	printf("%d", div);
+	if (b == 0) {
+		printf("Error");
+	}
+	else {
+		div = a / b;
+		printf("%d", div);
+	}
 	system("pause");
 	return 0;
 }
